9edc4975b0e12d6f889ceb428d4c50bfb1ddde10
MesselemYoucef <messelemy@gmail.com>
2022-12-03 21:38:12 +0400

    added the status bar

--- models/feedback_input.py
+++ models/feedback_input.py
@@ -15,3 +15,27 @@ class FeedbackInput(models.Model):
     work_email = fields.Char(related="employee_id.work_email")
     manager = fields.Char(related="employee_id.parent_id.name")
     coach = fields.Char(related="employee_id.coach_id.name")
+
+    input_status = fields.Selection([
+        ('draft', 'Draft'),
+        ('employee_approved', 'Employee Approved'),
+        ('manager_approved', 'Manager Approved'),
+        ('ceo_approved', 'CEO Approved'),
+        ('cancel', 'Cancel')
+    ], String="Status", default="draft", trackig=True)
+
+    def action_employee(self):
+        for rec in self:
+            rec.input_status = 'employee_approved'
+
+    def action_manager(self):
+        for rec in self:
+            rec.input_status = 'manager_approved'
+
+    def action_ceo(self):
+        for rec in self:
+            rec.input_status = 'ceo_approved'
+
+    def action_cancel(self):
+        for rec in self:
+            rec.input_status = 'cancel'
